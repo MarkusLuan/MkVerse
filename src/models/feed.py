81f28907e24a5d35b0ce2682f63413db4121d5b6
Markus Luan <markus23_luan@hotmail.com>
2024-10-26 00:42:41 -0300

    Corrigindo tipos das colunas na tabela de Feed

--- src/models/feed.py
+++ src/models/feed.py
@@ -4,10 +4,18 @@ from .abstract_model import AbstractModel
 from app_singleton import db
 
 class Feed (AbstractModel):
-    dt_remocao = db.Column(db.String, nullable=True)
-    texto = db.Column(DateTime, nullable=False)
+    fields = ["texto", "likes"]
+
+    dt_remocao = db.Column(DateTime, nullable=True)
+    texto = db.Column(db.String, nullable=False)
     uuid_imagem = db.Column(Uuid, unique=True, nullable=True)
     likes = db.Column(BigInteger, default=0, nullable=False)
     user_id = db.Column(BigInteger, ForeignKey("user.id"), nullable=False)
     
     user = db.relationship("User")
+
+    def __to_json(self):
+        j = super().to_json()
+
+        j["uuid_imagem"] = str(self.uuid_imagem) if self.uuid_imagem else None
+        return j
