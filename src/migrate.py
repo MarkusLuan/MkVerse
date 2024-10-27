0cc377d3870ca179c9ae4968eb7e9b75561a3d37
Markus Luan <markus23_luan@hotmail.com>
2024-10-26 23:09:52 -0300

    Mudando para Fake-Migrate - Não criava as tabelas
    E não tenho mais tempo pra ajeitar nada

--- src/migrate.py
+++ src/migrate.py
@@ -1,14 +1,20 @@
 import app
 import app_singleton
-import models
+from models import *
 
+import sys
 import argparse
 
-db = app_singleton.db
-migrate = app_singleton.migrate
+# db = app_singleton.db
+# migrate = app_singleton.migrate
 
-paser = argparse.ArgumentParser(exit_on_error=False)
-paser.add_argument("--config", default="config.development")
+# paser = argparse.ArgumentParser(exit_on_error=False)
+# paser.add_argument("--config", default="config.development")
 
-config_object = paser.parse_known_args()[0]
-app = app.create_app("config.development")
+# config_object = paser.parse_known_args()[0]
+config_object = sys.argv[1] if len(sys.argv) > 1 else 'config.development'
+app = app.create_app(config_object)
+
+with app.app_context():
+    app_singleton.db.drop_all()
+    app_singleton.db.create_all()
