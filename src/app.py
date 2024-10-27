b319108b5cf3b86f1adaa02ad08f5c1d22b719ee
Markus Luan <markus23_luan@hotmail.com>
2024-10-26 21:42:36 -0300

    Preparando para usar o flask migrate

--- src/app.py
+++ src/app.py
@@ -4,11 +4,10 @@ from flask_jwt_extended import JWTManager
 from models import *
 from resources import Resources
 from error_handler import ErrorHandler
+import app_singleton
 
 app = Flask(__name__)
 app.register_blueprint(Resources)
-
-import app_singleton
 
 app.config.from_object("config.development")
 if "DATABASE" in app.config:
@@ -23,11 +22,8 @@ if "DATABASE" in app.config:
 app_singleton.jwt.init_app(app)
 app_singleton.basic_auth.init_app(app)
 app_singleton.db.init_app(app)
+app_singleton.migrate.init_app(app, app_singleton.db)
 ErrorHandler(app)
-
-with app.app_context():
-    app_singleton.db.drop_all()
-    app_singleton.db.create_all()
 
 if __name__ == "__main__":
     app.run(
--- src/app_singleton.py
+++ src/app_singleton.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_basicauth import BasicAuth
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 jwt = JWTManager()
 basic_auth = BasicAuth()
+migrate = Migrate()
